a5957fcfe32008ddb860a7f4c60644b6a93bcbef
Samuel Dietz <samdietz2001@gmail.com>
2023-08-22 21:22:08 +1000

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <h1>Samuel Dietz</h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
